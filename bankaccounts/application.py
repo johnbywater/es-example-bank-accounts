4bcfeff2bc4fc9d62b8c291685124e6eae9db541
John Bywater <john.bywater@appropriatesoftware.net>
2020-05-12 17:27:50 +0100

    Added "deposit_funds()" command method.

--- bankaccounts/application.py
+++ bankaccounts/application.py
@@ -1,24 +1,32 @@
+from decimal import Decimal
+from uuid import UUID
+
 from eventsourcing.application.simple import SimpleApplication
 
 from bankaccounts.domainmodel import BankAccount
 
 
 class BankAccountApplication(SimpleApplication):
-    def create_account(self):
+    def create_account(self) -> UUID:
         account = BankAccount.__create__()
         self.save(account)
         return account.id
 
-    def get_balance(self, account_id1):
-        account = self.get_account(account_id1)
-        return account.balance
-
-    def get_account(self, account_id) -> BankAccount:
+    def get_account(self, account_id: UUID) -> BankAccount:
         account = self.repository[account_id]
         assert isinstance(account, BankAccount)
         return account
 
-    def tranfer_funds(self, debit_account_id, credit_account_id, amount):
+    def get_balance(self, account_id: UUID) -> Decimal:
+        account = self.get_account(account_id)
+        return account.balance
+
+    def deposit_funds(self, credit_account_id: UUID, amount: Decimal) -> None:
+        account = self.get_account(credit_account_id)
+        account.append_transaction(amount)
+        self.save(account)
+
+    def tranfer_funds(self, debit_account_id: UUID, credit_account_id: UUID, amount: Decimal) -> None:
         debit_account = self.get_account(debit_account_id)
         credit_account = self.get_account(credit_account_id)
         debit_account.append_transaction(-amount)
--- test_bank_accounts.py
+++ test_bank_accounts.py
@@ -10,17 +10,32 @@ class TestBankAccounts(TestCase):
     def test(self):
         with BankAccountApplication.mixin(PopoApplication)() as app:
             app: BankAccountApplication
+
+            # Create an account.
             account_id1 = app.create_account()
+
+            # Check balance.
+            self.assertEqual(app.get_balance(account_id1), Decimal("0.00"))
+
+            # Deposit funds.
+            app.deposit_funds(
+                credit_account_id=account_id1,
+                amount=Decimal("1000.00")
+            )
+
+            # Check balance.
+            self.assertEqual(app.get_balance(account_id1), Decimal("1000.00"))
+
+            # Create another account.
             account_id2 = app.create_account()
 
-            self.assertEqual(app.get_balance(account_id1), Decimal("0.00"))
-            self.assertEqual(app.get_balance(account_id2), Decimal("0.00"))
-
+            # Transfer funds.
             app.tranfer_funds(
                 debit_account_id=account_id1,
                 credit_account_id=account_id2,
                 amount=Decimal("100.00"),
             )
 
-            self.assertEqual(app.get_balance(account_id1), Decimal("-100.00"))
+            # Check balances.
+            self.assertEqual(app.get_balance(account_id1), Decimal("900.00"))
             self.assertEqual(app.get_balance(account_id2), Decimal("100.00"))

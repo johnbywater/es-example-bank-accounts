92ffaf74950cc05c23e258bcb3cf80678b0b3797
John Bywater <john.bywater@appropriatesoftware.net>
2020-05-13 02:18:02 +0100

    Improved type annotations.

--- bankaccounts/domainmodel.py
+++ bankaccounts/domainmodel.py
@@ -13,14 +13,14 @@ class BankAccount(BaseAggregateRoot):
         self.balance = Decimal("0.00")
         self.is_closed = False
 
-    def append_transaction(self, amount: Decimal):
+    def append_transaction(self, amount: Decimal) -> None:
         self.check_account_is_not_closed()
         self.check_has_sufficient_funds(amount)
         self.__trigger_event__(self.TransactionAppended, amount=amount)
 
     class TransactionAppended(BaseAggregateRoot.Event):
         @property
-        def amount(self):
+        def amount(self) -> Decimal:
             return self.__dict__["amount"]
 
         def mutate(self, obj: "BankAccount") -> None:
@@ -33,23 +33,23 @@ class BankAccount(BaseAggregateRoot):
         def mutate(self, obj: "BankAccount") -> None:
             obj.is_closed = True
 
-    def set_overdraft_limit(self, overdraft_limit):
+    def set_overdraft_limit(self, overdraft_limit: Decimal) -> None:
         assert overdraft_limit > Decimal("0.00")
         self.check_account_is_not_closed()
         self.__trigger_event__(self.OverdraftLimitSet, overdraft_limit=overdraft_limit)
 
     class OverdraftLimitSet(BaseAggregateRoot.Event):
         @property
-        def overdraft_limit(self):
+        def overdraft_limit(self) -> Decimal:
             return self.__dict__["overdraft_limit"]
 
         def mutate(self, obj: "BankAccount") -> None:
             obj.overdraft_limit = self.overdraft_limit
 
-    def check_account_is_not_closed(self):
+    def check_account_is_not_closed(self) -> None:
         if self.is_closed:
             raise AccountClosedError
 
-    def check_has_sufficient_funds(self, amount):
+    def check_has_sufficient_funds(self, amount: Decimal) -> None:
         if self.balance + amount < -self.overdraft_limit:
             raise InsufficientFundsError
